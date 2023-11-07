2ed94443ccca07ca9c374b2927e96a8c439eb4b2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-07 13:39:57 +0700

    add repository EmployeeRepository

---
+++ src/main/java/com/bayu/liquibase/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.bayu.liquibase.repository;
+
+import com.bayu.liquibase.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
