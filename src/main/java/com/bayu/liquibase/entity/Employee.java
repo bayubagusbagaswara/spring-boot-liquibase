010f43732f88dab45f083ed31a1e65db5be15231
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-07 13:37:44 +0700

    add entity Employee

---
+++ src/main/java/com/bayu/liquibase/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.bayu.liquibase.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "active")
+    private Boolean active;
+}
